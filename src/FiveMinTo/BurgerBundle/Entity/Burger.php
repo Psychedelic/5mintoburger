1dd62ea4e0c0d1f49b571c70ac281eef63b48948
Reivax <reivax.amok@gmail.com>
2013-05-12 13:19:27 +0200

    Modify the mapping for the entity Burger -> Vote for the id_vote

--- src/FiveMinTo/BurgerBundle/Entity/Burger.php
+++ src/FiveMinTo/BurgerBundle/Entity/Burger.php
@@ -13,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Burger
 {
 	/**
-	 * @ORM\OneToOne(targetEntity="FiveMinTo\BurgerBundle\Entity\Burger")
+	 * @ORM\OneToOne(targetEntity="FiveMinTo\BurgerBundle\Entity\Vote")
 	 */
 	 private $vote;
 	
